8916fe1c27bb62bf1dda937f81f45e81af807ecd
ElPerroCode <hm71054@gmail.com>
2024-06-04 07:34:15 -0500

    update 1

---
+++ app/src/main/java/com/example/parcial2/Activitys/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.parcial2.Activitys;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.parcial2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/example/parcial2/Entidades/User.java
@@ -0,0 +1,39 @@
+package com.example.parcial2.Entidades;
+
+public class User {
+    String name;
+    String number;
+    String id;
+
+
+    int pfp;
+    public User(String name, String number, String id) {
+        this.name = name;
+        this.number = number;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
